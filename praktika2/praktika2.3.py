3e1f047ef9846ad9f669fc1f52f5ab57592bb1a5
Artem Nikiforov <nikiforovartem2831@yandex.ru>
2023-03-19 11:47:05 +0300

    2.3

---
+++ praktika2/praktika2.3.py
@@ -0,0 +1,39 @@
+import cv2
+import argparse
+import numpy as np
+import imutils
+
+def task():
+    path = './praktika2/Image2.3/'
+
+    def set_image(image):
+        return cv2.imread(path + image)
+    
+    def fragment_search():
+        image = set_image('image.jpeg')
+        
+        fragment = set_image('fragment.jpg')
+
+        (fragmentHeight, fragmentWidth) = fragment.shape[:2]
+        # Осуществляем поиск фрагмента на изображении
+        result = cv2.matchTemplate(image, fragment, cv2.TM_CCOEFF)
+        (_, _, minLoc, maxLoc) = cv2.minMaxLoc(result)
+        # Выделяем искомый фрагмент и отделяем его от изображения
+        topLeft = maxLoc
+        botRight = (topLeft[0] + fragmentWidth, topLeft[1] + fragmentHeight)
+        roi = image[topLeft[1]:botRight[1], topLeft[0]:botRight[0]]
+        # Создаем затемненный прозрачный слой, для затемнения всей части изображения
+        mask = np.zeros(image.shape, dtype="uint8")
+        image = cv2.addWeighted(image, 0.20, mask, 0.75, 0)
+        # Возвращаем искомый фрагмент в изображение
+        image[topLeft[1]:botRight[1], topLeft[0]:botRight[0]] = roi
+        # Вывод фрагмента и изображения в отдельные окна (закрываются при нажатии любой клавиши на клавиатуре)
+        cv2.imshow(f"Image", imutils.resize(image, height=900))
+        cv2.imshow(f"fragment", fragment)
+        cv2.waitKey(0)
+
+        cv2.imwrite(f"./praktika2/result2.3/fragment_search.jpg", image)
+        
+    fragment_search()
+
+task()
